43267f99e64518ab0fbf0cac6a741466426bf6e0
Usuario <marprogr@protonmail.com>
2021-12-10 19:23:15 -0300

    updated the classes for room and gim lessons

--- gimnasio/gim/models.py
+++ gimnasio/gim/models.py
@@ -1,3 +1,38 @@
 from django.db import models
-
 # Create your models here.
+class GimRoom(models.Model):
+    """Data for each room of the gim."""
+    id = models.AutoField(primary_key=True)
+    location = models.CharField(max_length=45)
+    area = models.IntegerField()
+    room_type = models.CharField(max_length=45)
+    def __str__(self) -> str:
+        return f"{self.room_type} {self.location}" 
+
+class GimClass(models.Model):
+    """Data for the gim class."""
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=45)
+    datetime = models.DateTimeField()
+    room = GimRoom()
+
+    def __str__(self) -> str:
+        return self.description
+
+class GimTeacher(models.Model):
+    """Data of each gim teacher."""
+    dni = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    phone_number = models.PositiveBigIntegerField()
+    hiring_date = models.DateField()
+    birthdate = models.DateField()
+    
+class GimClient(models.Model):
+    affiliate_number = models.IntegerField(primary_key=True)
+    profession = models.CharField(max_length=45)
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    address = models.TextField(max_length=45)
+    phone_number = models.PositiveBigIntegerField()
+    is_up_to_date = models.BooleanField()
--- gimnasio/gim/views.py
+++ gimnasio/gim/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from .models import GimClass, GimClient
+def index(request):
+    """The home page for the gim page."""
+    return render(request, 'gimnasio/base.html')
 
-# Create your views here.
+def classes(request):
+    """Show list of available classes."""
+    context = GimClass.objects.get()
+    return render(request, 'gimnasio/Clases.html', context)
+
+@login_required
+def clients(request):
+    """Show all clients of the gym, only accesible to gym staff."""
+    return render(request, 'gimnasio/Clientes.html')
